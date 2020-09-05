47e7a6b36593fb46d975d595aced1c40e14dcafa
farkaskid <sid.shishu@gmail.com>
2020-09-05 17:50:21 +0530

    Added spring data jpa and sprint data rest

---
+++ src/main/java/com/velotio/demo1/UserRepository.java
@@ -0,0 +1,11 @@
+package com.velotio.demo1;
+
+import java.util.List;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    List<User> findByLastName(@Param("name") String name);
+}
